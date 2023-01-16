e544c269e97270067aae4ea7620481130f379f28
Julia <yulya.strakhova.1992@mail.ru>
2023-01-16 17:15:47 +0700

    soon finaly

--- src/main/java/com/mycompany/sql/SQLint.java
+++ src/main/java/com/mycompany/sql/SQLint.java
@@ -5,16 +5,18 @@
 package com.mycompany.sql;
 
 import java.sql.Connection;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  *
  * @author Student
  */
 public interface SQLint extends AutoCloseable{
-    Connection connect (String url, String username, String password);
+    boolean connect (String url, String username, String password);
     boolean createTable(String tableName, String[] params);
-    int insert (String[] [] values);
-    int updateRows (String tableName, String column, String conditionColumn, String[] conditions, String values);
+    int insert (String[] [] values, String tableName, String[] columns);
+    int updateRows (String tableName, String column, String conditionColumn, String[] conditions, String [] values);
     // UPDATE students SET name = CASE id
     //                            WHEN 23 THEN 'Alexandr'
     //                            WHEN 24 THEN 'Sasha'
@@ -23,7 +25,7 @@ public interface SQLint extends AutoCloseable{
     // WHERE id INT (23,24,25);
     boolean updateColumns (String tableName, String [] columns, String[] values, String condition);
     // UPDATE students SET name = 'Alexandr', date = 12 WHERE school_id = 23 AND last_name LIKE 'smth';
-    String [] select (String values);
+    LinkedList <LinkedList <String>> select (String values, String tableName);
     boolean delete (String tableName);
     int deleteRows (String tableName, String condition);
     boolean addColumn (String tableName, String column);// column = "name TEXT NOT NULL"
